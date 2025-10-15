7aa9519474213b6f7357711103fd81d7e5e210b8
ernestizic <ieifeanyichukwu@gmail.com>
2025-10-15 14:36:24 +0100

    fix: blocked set-cookie header on prod

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -46,7 +46,7 @@ export class AuthController {
     res.cookie('access_token', access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
@@ -70,7 +70,7 @@ export class AuthController {
     res.cookie('access_token', access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
